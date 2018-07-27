4359efbd751ff91fe9759afd2a9c10793f38cf8c
vijayisadba <34603780+vijayisadba@users.noreply.github.com>
2018-07-27 20:02:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Compilation:  javac HelloWorld.java
+ *  Compilation :  javac HelloWorld.java
  *  Execution:    java HelloWorld
  *
  *  Prints "Hello, World". By tradition, this is everyone's first program.
